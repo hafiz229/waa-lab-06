119a24518f839e5551e7c5311f716ef926eca7aa
A H M Golam Hafiz <hasnathafiz229@gmail.com>
2024-10-11 10:46:33 -0500

    fetch the default list from server side

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import avatar from "./images/bozai.png";
 import dayjs from "dayjs";
 import { v4 as uuidv4 } from "uuid";
@@ -19,47 +19,46 @@ interface Comment {
   like: number;
 }
 
-// Comment List data
-const defaultList = [
-  {
-    // comment id
-    rpid: "3",
-    // user info
-    user: {
-      uid: "13258165",
-      avatar: "",
-      uname: "Jay Zhou",
-    },
-    // comment content
-    content: "Nice, well done",
-    // created datetime
-    ctime: "10-18 08:15",
-    like: 88,
-  },
-  {
-    rpid: "2",
-    user: {
-      uid: "36080105",
-      avatar: "",
-      uname: "Song Xu",
-    },
-    content: "I search for you thousands of times, from dawn till dusk.",
-    ctime: "11-13 11:29",
-    like: 88,
-  },
-  {
-    rpid: "1",
-    user: {
-      uid: "30009257",
-      avatar,
-      uname: "John",
-    },
-    content:
-      "I told my computer I needed a break... now it will not stop sending me vacation ads.",
-    ctime: "10-19 09:00",
-    like: 66,
-  },
-];
+// const defaultList = [
+//   {
+//     // comment id
+//     rpid: "3",
+//     // user info
+//     user: {
+//       uid: "13258165",
+//       avatar: "",
+//       uname: "Jay Zhou",
+//     },
+//     // comment content
+//     content: "Nice, well done",
+//     // created datetime
+//     ctime: "10-18 08:15",
+//     like: 88,
+//   },
+//   {
+//     rpid: "2",
+//     user: {
+//       uid: "36080105",
+//       avatar: "",
+//       uname: "Song Xu",
+//     },
+//     content: "I search for you thousands of times, from dawn till dusk.",
+//     ctime: "11-13 11:29",
+//     like: 88,
+//   },
+//   {
+//     rpid: "1",
+//     user: {
+//       uid: "30009257",
+//       avatar,
+//       uname: "John",
+//     },
+//     content:
+//       "I told my computer I needed a break... now it will not stop sending me vacation ads.",
+//     ctime: "10-19 09:00",
+//     like: 66,
+//   },
+// ];
 // current logged in user info
 const user = {
   // userid
@@ -71,12 +70,6 @@ const user = {
 };
 
 const currentUser = user;
-
-// Nav Tab
-// const tabs = [
-//   { type: "hot", text: "Top" },
-//   { type: "newest", text: "Newest" },
-// ];
 
 // Method for sorting comments
 const getSortedComments = (commentList: Comment[], sortBy: string) => {
@@ -93,9 +86,19 @@ const getSortedComments = (commentList: Comment[], sortBy: string) => {
 };
 
 const App = () => {
-  const [commentList, setCommentList] = useState<Comment[]>(defaultList);
+  const [commentList, setCommentList] = useState<Comment[]>([]);
   const [tabType, setTabType] = useState<string>("");
   const [content, setContent] = useState<string>("");
+
+  const fetchCommentList = async () => {
+    const response = await fetch("http://localhost:3001/comments");
+    const data = await response.json();
+    setCommentList(data)
+  }
+
+  useEffect(() => {
+    fetchCommentList();
+  }, []);
 
   const onDeleteComment = (id: string) => {
     const updatedCommentList = commentList?.filter(({ rpid }) => rpid !== id);
